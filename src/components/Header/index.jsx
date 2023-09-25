a0e79d1c704168d83a3dc94e1cc9e3c42e969493
Fiona0710 <berthou.fiona@hotmail.fr>
2023-09-25 18:42:03 +0200

    Creating the Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+
+import { Link} from 'react-router-dom'; 
+import ArgentBankLogo from '../../assets/argentBankLogo.png';
+import './index.css'
+
+export default function Header() {
+    
+
+  return (
+    <nav className="main-nav">
+    <Link to="/" className="main-nav-logo">
+      <img
+        className="main-nav-logo-image"
+        src={ArgentBankLogo}
+        alt="Argent Bank Logo"
+      />
+      <h1 className="sr-only">Argent Bank</h1>
+    </Link>
+    <div>
+    <Link to="/SignIn" className="main-nav-item">
+        <i className="fa fa-user-circle"></i>
+        {" "}Sign In
+    </Link>
+    </div>
+  </nav>                  
+  );
+}
