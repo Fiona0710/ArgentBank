d4d929da272811e59daab962fb14a6824c30e878
Fiona0710 <berthou.fiona@hotmail.fr>
2023-09-26 19:48:37 +0200

    Add components: FormContainer, FormField and Button in the return of the SignIn page

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,8 +1,33 @@
+import FormContainer from '../../components/FormContainer'
+import FormField from '../../components/FormField';
+import Button from '../../components/Button';
 
 export default function SignIn() {
     return (
-      <div>
-        {/* Contenu de la page SignIn */}
-      </div>
+      <FormContainer>
+        <FormField
+          className='input-wrapper'
+          id='username'
+          label='Username'
+          type='text'
+        />
+        <FormField
+          className='input-wrapper'
+          id='password'
+          label='Password'
+          type='password'
+        />
+        <FormField
+          className='input-remember'
+          id='remember-me'
+          label='Remember-me'
+          type='checkbox'
+        />
+        <Button
+          className='sign-in-button'
+          name='Sign In'
+        />
+      </FormContainer>
     );
   }
+  
