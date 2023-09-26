d7891d40a26d2b2fde92229566a9d1fbdddae25d
Fiona0710 <berthou.fiona@hotmail.fr>
2023-09-26 19:37:16 +0200

    Using useLocation() to display or not a class on the main of the SignIn page

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,17 +1,22 @@
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation} from 'react-router-dom';
 import './index.css'
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
+
+
 export default function Layout() {
+  const location = useLocation();
+  const mainClassName = location.pathname === '/SignIn' ? 'main bg-dark' : '';
+
   return (
-    <div>
+    <>
       <Header />
-      <main>
+      <main className={mainClassName}>
         <Outlet /> {/* Renders the child routes */}
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
