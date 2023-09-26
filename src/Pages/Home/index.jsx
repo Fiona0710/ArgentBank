7ce184c64ca927a93ff8f9a0f728da32ed1408ab
Fiona0710 <berthou.fiona@hotmail.fr>
2023-09-26 15:51:25 +0200

    Calling the FeatureItem component in the Home page with a .map

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,23 @@
 import Banner from '../../components/Banner'
+import FeatureItem from '../../components/FeatureItem';
+import features from '../../components/FeatureItem/Features'
 
-function Home() {
+export default function Home() {
   return (
     <div>
       <Banner />
-      {/* Contenu de la page d'accueil */}
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        {features.map((feature, index) => (
+          <FeatureItem 
+            key={index} 
+            image={feature.image}
+            alt={feature.alt} 
+            title={feature.title} 
+            content={feature.content} 
+          />    
+        ))}  
+      </section>
     </div>
   );
 }
-
-export default Home;
