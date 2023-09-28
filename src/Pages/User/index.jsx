23ca71f493bd1551dc7fe1199641c0fdeb571e7a
Fiona0710 <berthou.fiona@hotmail.fr>
2023-09-28 18:19:22 +0200

    Integration of the User page using a map on the Account and Button components in the return

--- src/Pages/User/index.jsx
+++ src/Pages/User/index.jsx
@@ -1,8 +1,33 @@
+import Button from '../../components/Button'
+import Account from '../../components/Account';
+import accountInformation from '../../data/accountInformation';
 
 export default function User() {
     return (
-      <div>
-        {/* Contenu de la page User */}
+      <>
+      <div className="header">
+        <h1>Welcome back<br />Tony Jarvis!</h1>
+        <Button
+          className={'edit-button'}
+          name={'Edit Name'}
+        />
       </div>
+      <h2 className="sr-only">Accounts</h2>
+      {accountInformation.map((info, index) => (
+          <Account
+            key={index} 
+            title={info.title}
+            amount={info.amount} 
+            description={info.description} 
+          >
+            <Button
+              className={'transaction-button'}
+              name={'View transactions'}
+             />
+          </Account>    
+        ))}  
+      </>
     );
   }
+
+
