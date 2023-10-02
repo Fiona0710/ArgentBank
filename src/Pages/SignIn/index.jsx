b55912a4fccd729351ae36d1e96bc2d3038473ed
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-02 20:13:28 +0200

    Removing remember-me logic

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,21 +1,62 @@
-import FormContainer from '../../components/FormContainer'
+import FormContainer from '../../components/FormContainer';
 import FormField from '../../components/FormField';
 import Button from '../../components/Button';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null);
+
+ 
+  
+ async function submitForm(event) {
+    event.preventDefault();
+          
+    try {
+      const response =  await fetch (`http://localhost:3001/api/v1/user/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})  
+          });
+
+      if (response.status === 200) {
+      
+        const responseData = await response.json();
+        const token = responseData.body.token;
+        window.localStorage.setItem('token', token);
+        navigate("/User"); 
+        
+      } else {
+        setError('Oups, il y a eu un problème');
+      }
+    } catch (error) {
+      setError('Oups, il y a eu une erreur de réseau');
+    }
+}
+            
     return (
-      <FormContainer>
+      <FormContainer onSubmit={submitForm}>
+        {error && <span>{error}</span>}
         <FormField
           className='input-wrapper'
           id='username'
-          label='Username'
+          label='Email'
           type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}  
         />
         <FormField
           className='input-wrapper'
           id='password'
           label='Password'
           type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
         />
         <FormField
           className='input-remember'
@@ -26,8 +67,8 @@ export default function SignIn() {
         <Button
           className='sign-in-button'
           name='Sign In'
+          type='submit'  
         />
       </FormContainer>
     );
   }
-  
