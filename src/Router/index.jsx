5684ba1d290c561f4675de786b3c94cfce764cb3
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-02 20:06:04 +0200

    Adding a ProtectedRoute component to secure the User page

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Home from '../Pages/Home';
 import SignIn from '../Pages/SignIn'; 
 import User from '../Pages/User';
+import ProtectedRoute from '../components/ProtectedRoute';
 
-
+  const token = window.localStorage.getItem("token");
 export default function Router() {
+  
   return (
     // Use the BrowserRouter component to define routing
      <BrowserRouter>
@@ -13,8 +15,15 @@ export default function Router() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="SignIn" element={<SignIn />} />
-          <Route path="User" element={<User />} />
-        </Route>  
+          <Route path="User"
+            element={
+              <ProtectedRoute token={token}>
+                <User />
+              </ProtectedRoute>
+            }
+          />  
+          <Route path="*" element= {<SignIn/>} />  
+        </Route>    
       </Routes>
     </BrowserRouter>
     
