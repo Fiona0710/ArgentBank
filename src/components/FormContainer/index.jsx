dc3594e2182b400f01691b02f25ebdd5f209a89f
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-02 20:12:12 +0200

    Adding props to FormField and FormContainer components

--- src/components/FormContainer/index.jsx
+++ src/components/FormContainer/index.jsx
@@ -1,10 +1,10 @@
 
-export default function FormContainer ({children}) {
+export default function FormContainer ({children, onSubmit}) {
     return (
         <section className="sign-in-content">
             <i className="fa fa-user-circle sign-in-icon"></i>
             <h1>Sign In</h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 {children}
             </form>
         </section>
--- src/components/FormField/index.jsx
+++ src/components/FormField/index.jsx
@@ -1,9 +1,14 @@
 
-export default function FormField ({className, id, type, label}) {
+export default function FormField ({className, id, type, label, onChange, value}) {
     return(
           <div className={className}>
             <label htmlFor={id}>{label}</label>
-            <input type={type} id={id} />
+            <input 
+            type={type} 
+            id={id}
+            onChange={onChange}
+            value={value}
+             />
           </div>
     )
 }
