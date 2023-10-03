93275f155a4f63283d078212fd00fc236ff61e4a
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-03 12:57:40 +0200

    Form submission logic moved to a hooks folder

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,43 +1,11 @@
 import FormContainer from '../../components/FormContainer';
 import FormField from '../../components/FormField';
 import Button from '../../components/Button';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useSignIn } from '../../hooks/useSignIn';
 
 export default function SignIn() {
-  const navigate = useNavigate();
-  const [error, setError] = useState(null);
-  
- async function submitForm(event) {
-    event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-       
-    try {
-      const response =  await fetch (`http://localhost:3001/api/v1/user/login`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email, password})  
-          });
-
-      if (response.status === 200) {
-      
-        const responseData = await response.json();
-        const token = responseData.body.token;
-         
-        window.localStorage.setItem('token', token); 
-        navigate("/User"); 
-      } else {
-        setError('Oups, il y a eu un problème');
-      }
-    } catch (error) {
-      setError('Oups, il y a eu une erreur de réseau');
-    }
-   
-}
-            
+  const { error, submitForm } = useSignIn();
+           
     return (
       <FormContainer onSubmit={submitForm}>
         {error && <span>{error}</span>}
