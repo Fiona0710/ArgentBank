6324709228b2f370e47cf87e688fde7319d2577b
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-08 21:14:51 +0200

    Setting up the redux provider and store

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -4,26 +4,28 @@ import Home from '../Pages/Home';
 import SignIn from '../Pages/SignIn'; 
 import User from '../Pages/User';
 import ProtectedRoute from '../components/ProtectedRoute';
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 export default function Router() {
   
   return (
-    // Use the BrowserRouter component to define routing
+    <Provider store={store}>
      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/User"
-            element={ <ProtectedRoute >
-                        <User />
-                      </ProtectedRoute> 
-            }
-          />  
-          <Route path="*" element= {<SignIn/>} />  
-        </Route>    
-      </Routes>
-    </BrowserRouter>
-    
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/User"
+              element={ <ProtectedRoute >
+                          <User />
+                        </ProtectedRoute> 
+              }
+            />  
+            <Route path="*" element= {<SignIn/>} />  
+          </Route>    
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
