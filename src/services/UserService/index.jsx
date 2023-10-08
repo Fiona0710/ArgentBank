734079f84e478ad70488c015ac711c65c5071648
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-08 21:16:35 +0200

    Removing localStorage and changing return to true a token

--- src/services/UserService/index.jsx
+++ src/services/UserService/index.jsx
@@ -1,10 +1,9 @@
 import { fetchApi } from '../../utils/fetchApi';
 
 export async function signInFetch(email, password) {
+
   const responseData = await fetchApi('/login', 'POST', { email, password }, false);
   
   const token = responseData.body.token;
-  window.localStorage.setItem('token', token);
-  
-  return true;
+  return token;
 }
