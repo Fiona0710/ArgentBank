0403be1a8b7879c11f928b729233a2fc0ba8c05a
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-10 14:48:32 +0200

    Added a div instead of the fragment to add the className (main bg-dark) to the User page.

--- src/Pages/User/index.jsx
+++ src/Pages/User/index.jsx
@@ -1,47 +1,42 @@
 import Button from '../../components/Button'
 import Account from '../../components/Account';
 import accountInformation from '../../data/accountInformation';
+// import { useDataProfile } from '../../hooks/useProfile';
 import { profilFetch } from '../../services/UserService';
 import { useDispatch, useSelector } from 'react-redux'; 
 import { userProfile } from '../../redux/profileSlice';
-//   import { selectUserData } from '../../redux/selectors';
-  // import { useEffect } from 'react';
-
-   
-   
+import { selectUserData } from '../../redux/selectors';
+import { selectAuthToken } from '../../redux/selectors';
 
   
 export default function User() {
-async function fetchData() {
+  // const data = useSelector(selectUserData)
+  //   console.log(data)
+  // const {fetchData } = useDataProfile();
+  // fetchData();
+ const data = useSelector(selectUserData)
+  console.log(data) 
+  
+  async function fetchData() {
+  const token = useSelector(selectAuthToken);
     const dispatch = useDispatch();
      try {
-       const userData = await profilFetch();
+       const userData = await profilFetch(token);
 
         console.log(userData)
-        dispatch(userProfile(userData))
-      
-    
-      }  
-      
-       
+        dispatch(userProfile(userData.body))
+      }   
       catch (error) {
        // Gérer les erreurs
      }
    }
 
 fetchData()
- 
- 
-
-
-  
-
-// {data.firstName} {data.lastName}
 
     return (
-      <>
+      <div  className='main bg-dark'>
       <div className="header">
-        <h1>Welcome back<br /></h1>
+        <h1>Welcome back<br />{data.firstName} {data.lastName}</h1>
         <Button
           className={'edit-button'}
           name={'Edit Name'}
@@ -61,7 +56,7 @@ fetchData()
              />
           </Account>    
         ))}  
-      </>
+      </div>
     );
   }
 
