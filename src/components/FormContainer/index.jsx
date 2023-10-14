9a3572437420a41ddc7de2cb6057a89408bed452
Fiona0710 <berthou.fiona@hotmail.fr>
2023-10-14 15:26:06 +0200

    Adding the title prop

--- src/components/FormContainer/index.jsx
+++ src/components/FormContainer/index.jsx
@@ -1,9 +1,9 @@
 
-export default function FormContainer ({children, onSubmit}) {
+export default function FormContainer ({children, onSubmit,title}) {
     return (
         <section className="sign-in-content">
             <i className="fa fa-user-circle sign-in-icon"></i>
-            <h1>Sign In</h1>
+            <h1>{title}</h1>
             <form onSubmit={onSubmit}>
                 {children}
             </form>
